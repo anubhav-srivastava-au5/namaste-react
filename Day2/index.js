4677c0ee1a403e23530998f6e53ed5e769b31eab
anubhav <anubhav359@gmail.com>
2023-07-12 20:24:37 +0530

    updated day2 assignment

---
+++ Day2/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const hello1=React.createElement('h1',{},"Hi! Anubhav");
+const hello2=React.createElement('h4',{},"Hi! again!!")
+const container=React.createElement('div',{
+    id:"container"
+},[hello1,hello2])
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(container);
