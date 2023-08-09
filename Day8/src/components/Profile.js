f3704e7c78e9e726da766db1a2e5f7adfc8c9ea8
anubhav <anubhav359@gmail.com>
2023-08-09 23:37:28 +0530

    Day 8 assignment added

--- Day8/src/components/Profile.js
+++ Day8/src/components/Profile.js
@@ -1,9 +1,25 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
 
 const Profile = (props) =>{
 
-    const [count,setCount]=useState(0)
+    const [count,setCount]=useState(0);
+    console.log("state profile functional")
+    useEffect(()=>{
+        console.log("useeffect profile functional")
+       const timer=setInterval(() => {
+            console.log("namaste react op")
+        }, 1000);
+
+        return () =>{
+            console.log("return profile functional")
+            clearInterval(timer)
+        }  //it is unmounting as of class component
+         
+
+        
+    },[])
     return (
+
         <div>
             <h1>Profile Functional Component Done by {props.name}</h1>
             <h3>Counter: {count}</h3>
--- Day8/src/components/ProfileClass.js
+++ Day8/src/components/ProfileClass.js
@@ -2,26 +2,51 @@ import React from 'react';
 
 class Profile extends React.Component {
     constructor(props){
+        console.log("child constructor");
         super(props);
         //create state 
         this.state={
             count:0,
+            name:"anubhav"
         }
 
     }
+    async componentDidMount(){
+        const data=await fetch("https://api.github.com/users/anubhav-srivastava-au5");
+        const json=await data.json();
+        console.log(json,"-p----");
+        console.log("child component did mount");
+        this.setState({
+            name:json.login
+        })
+
+        // this.timer=setInterval(() => {
+        //     console.log("namaste react")
+        // }, 1000);
+
+    }
+    componentDidUpdate(){
+        console.log("child component did update");
+    }
+    componentWillUnmount(){
+        // clearInterval(this.timer)
+        console.log("child component will unmount");
+    }
     render() {
+        console.log("child render")
         const {count}=this.state;
     return (
     <div>
     <h1>Profile class Component by {this.props.name}</h1>
+    <h3>Profile class Component by {this.state.name}</h3>
     {/* <h3>Count : {this.state.count}</h3> //other way without destructring */}
     <h3>Count : {count}</h3>
     <button onClick={()=>{
-        this.state.count=1
+        // this.state.count=1
         
-        // this.setState({
-        //     count:1
-        // })
+        this.setState({
+            count:1
+        })
     }}>click me!</button>
     </div>
     )
